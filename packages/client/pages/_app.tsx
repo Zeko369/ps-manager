f668f6649489d62db50a0258493f69da02cd11dd
Fran Zekan <zekan.fran369@gmail.com>
2020-07-26 09:48:30 +0200

    Testing apollo

--- packages/client/pages/_app.tsx
+++ packages/client/pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ApolloClient, NormalizedCacheObject } from '@apollo/client';
 import { AppProps as BaseAppProps } from 'next/app';
+import Link from 'next/link';
 
 import { withApollo } from '../lib/apollo';
 
@@ -12,6 +13,8 @@ const App: React.FC<AppProps> = ({ Component, pageProps, apollo }) => {
   return (
     <div>
       <h1>Hello</h1>
+      <Link href="/">Home</Link>
+      <Link href="/other">Other</Link>
       <Component {...pageProps} />
     </div>
   );
---
+++ packages/client/pages/other.tsx
@@ -0,0 +1,41 @@
+import { NextPage } from 'next';
+import { gql, useQuery } from '@apollo/client';
+
+const QUERY = gql`
+  query Main {
+    users {
+      id
+      email
+    }
+  }
+`;
+
+const Home: NextPage = () => {
+  const { loading, error, data } = useQuery(QUERY);
+
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
+  if (error) {
+    return (
+      <>
+        <h2>error...</h2>
+        <code>{error}</code>
+      </>
+    );
+  }
+
+  return (
+    <div>
+      <h2>Users: </h2>
+      <ul>
+        {data.users.map((user) => (
+          <li key={user.id}>{user.email}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Home;
