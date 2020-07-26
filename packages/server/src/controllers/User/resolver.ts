ba208fc4286728e9636c66f26f22e1523edce92c
Fran Zekan <zekan.fran369@gmail.com>
2020-07-26 22:44:11 +0200

    Cleanup users resolvers

--- packages/server/src/controllers/User/resolver.ts
+++ packages/server/src/controllers/User/resolver.ts
@@ -1,6 +1,5 @@
 import { Resolver, Query, Mutation, Arg, Int } from 'type-graphql';
 import { User } from '../../models/User';
-import { CreateUserInputs, UpdateUserInput } from './inputs';
 
 @Resolver()
 export class UserResolver {
@@ -12,28 +11,6 @@ export class UserResolver {
   @Query(() => User)
   user(@Arg('id', () => Int) id: number) {
     return User.findOne({ where: { id } });
-  }
-
-  @Mutation(() => User)
-  async createUser(@Arg('data') data: CreateUserInputs) {
-    const user = User.create(data);
-    await user.save();
-    return user;
-  }
-
-  @Mutation(() => User)
-  async updateUser(@Arg('id', () => Int) id: number, @Arg('data') data: UpdateUserInput) {
-    const user = await User.findOne({ where: { id } });
-
-    if (!user) {
-      throw new Error('User not found!');
-    }
-
-    user.email = data.email ?? user.email;
-    user.password = data.password ?? user.password;
-
-    await user.save();
-    return user;
   }
 
   @Mutation(() => Boolean)
