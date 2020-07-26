bdceb25f68771705f1a05277befa5b9200cb44c8
Fran Zekan <zekan.fran369@gmail.com>
2020-07-26 22:44:18 +0200

    Base auth resolvers

---
+++ packages/server/src/controllers/Auth/resolver.ts
@@ -0,0 +1,41 @@
+import bcrypt from 'bcryptjs';
+import { Resolver, Arg, Mutation } from 'type-graphql';
+import { User } from '../../models/User';
+import { SignUpInput } from './input';
+
+@Resolver()
+export class AuthResolver {
+  @Mutation(() => User)
+  async signUp(@Arg('data') data: SignUpInput) {
+    const { email, password, firstName, lastName } = data;
+    const lowerEmail = email.toLowerCase();
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    let user = await User.findOne({ where: { email: lowerEmail } });
+    if (user) {
+      throw new Error('EMAIL_IN_USE/Email already in use');
+    }
+
+    user = new User({ email: lowerEmail, password: hashedPassword, firstName, lastName });
+    await user.save();
+
+    return user;
+  }
+
+  @Mutation(() => User)
+  async signIn(@Arg('email') email: string, @Arg('password') password: string) {
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error('EMAIL_NOT_FOUND/Cant find user with that email');
+    }
+
+    const valid = await bcrypt.compare(password, user.password);
+
+    if (!valid) {
+      throw new Error('INVALID_PASSWORD/Incorrect email address or password.');
+    }
+
+    return user;
+  }
+}
--- packages/server/src/controllers/index.ts
+++ packages/server/src/controllers/index.ts
@@ -1,6 +1,7 @@
 import { UserResolver } from './User/resolver';
 import { ProductResolver } from './Product/resolver';
+import { AuthResolver } from './Auth/resolver';
 
-const resolvers = [UserResolver, ProductResolver] as const;
+const resolvers = [UserResolver, ProductResolver, AuthResolver] as const;
 
 export default resolvers;
