6891c157078aec2b2b3d8148f6a7b13bcf242647
Fran Zekan <zekan.fran369@gmail.com>
2020-07-27 16:26:21 +0200

    Add me / signOut query/mutation

--- packages/server/src/controllers/Auth/resolver.ts
+++ packages/server/src/controllers/Auth/resolver.ts
@@ -1,11 +1,19 @@
 import bcrypt from 'bcryptjs';
-import { Resolver, Arg, Mutation } from 'type-graphql';
+import jwt from 'jsonwebtoken';
+import { Resolver, Arg, Mutation, Query, Ctx } from 'type-graphql';
+
 import { User } from '../../models/User';
 import { SignUpInput } from './input';
 import { hash } from '../../lib/hash';
+import { GQLCtx } from '../../ts/gql';
 
 @Resolver()
 export class AuthResolver {
+  @Query(() => User, { nullable: true })
+  async me(@Ctx() ctx: GQLCtx) {
+    return ctx.user;
+  }
+
   @Mutation(() => User)
   async signUp(@Arg('data') data: SignUpInput) {
     const { email, password, firstName, lastName } = data;
@@ -24,7 +32,7 @@ export class AuthResolver {
   }
 
   @Mutation(() => User)
-  async signIn(@Arg('email') email: string, @Arg('password') password: string) {
+  async signIn(@Arg('email') email: string, @Arg('password') password: string, @Ctx() ctx: GQLCtx) {
     const user = await User.findOne({ where: { email } });
 
     if (!user) {
@@ -37,6 +45,22 @@ export class AuthResolver {
       throw new Error('INVALID_PASSWORD/Incorrect email address or password.');
     }
 
+    const token = jwt.sign({ userId: user.id }, 'hello-world');
+    ctx.res.cookie('token', token, {
+      httpOnly: false,
+      maxAge: 1000 * 60 * 60 * 24 * 365 // year
+    });
+
     return user;
+  }
+
+  @Mutation(() => Boolean)
+  async signOut(@Ctx() ctx: GQLCtx) {
+    if (ctx.user) {
+      ctx.res.clearCookie('token');
+      return true;
+    }
+
+    return false;
   }
 }
