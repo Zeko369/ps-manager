7b58daa6262b764f42ef30b21d59d864a81c24fe
Fran Zekan <zekan.fran369@gmail.com>
2020-07-27 15:35:36 +0200

    Refactor to apollo-server-express

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,16 +1,26 @@
 import 'reflect-metadata';
+import express from 'express';
+import cors from 'cors';
 import { createConnection } from 'typeorm';
-import { ApolloServer } from 'apollo-server';
+import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 
 import resolvers from './controllers';
 
 async function main() {
   const connection = await createConnection();
+  const app = express();
+
+  app.use(cors());
+
   const schema = await buildSchema({ resolvers, validate: false });
   const server = new ApolloServer({ schema });
-  await server.listen(4000);
-  console.log('Listening on http://localhost:4000');
+
+  server.applyMiddleware({ app, cors: false });
+
+  app.listen(4000, () => {
+    console.log('Listening on http://localhost:4000');
+  });
 }
 
 main();
